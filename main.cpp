7f6a6dd420de42120f003249f0aa186361494eaa
kani537 <94041681+kani537@users.noreply.github.com>
2023-12-08 23:22:08 +0900

    double version

--- main.cpp
+++ main.cpp
@@ -1,10 +1,4 @@
 #include <Siv3D.hpp>
-#include <Siv3D/Profiler.hpp>
-#include <Siv3D/Scene.hpp>
-#include <Siv3D/System.hpp>
-#include <Siv3D/Unicode.hpp>
-
-#include "fraction/fraction.hpp"
 
 struct obj {
   enum class obj_type {
@@ -13,8 +7,8 @@ struct obj {
     KINEMATIC
   };
 
-  obj(frac x, frac y, frac w, frac h, frac mass) {
-    velocity = frac(0);
+  obj(double x, double y, double w, double h, double mass) {
+    velocity = 0;
     _x = x;
     _y = y;
     _w = w;
@@ -22,7 +16,7 @@ struct obj {
     _m = mass;
   }
 
-  obj setVelocity(frac _velocity) {
+  obj setVelocity(double _velocity) {
     velocity = _velocity;
     return *this;
   }
@@ -33,15 +27,15 @@ struct obj {
   }
 
   Rect rect(void) {
-    return Rect(_x.calc(), _y.calc(), _w.calc(), _h.calc());
+    return Rect(_x, _y, _w, _h);
   }
 
-  frac velocity;
-  frac _x;
-  frac _y;
-  frac _w;
-  frac _h;
-  frac _m;
+  double velocity;
+  double _x;
+  double _y;
+  double _w;
+  double _h;
+  double _m;
   obj_type _type = obj_type::STATIC;
 };
 
@@ -64,61 +58,60 @@ std::set<std::pair<int, int>> getCollisions(const Array<obj> &objs) {
   return collisions;
 }
 
-void calcCollisions(Array<obj> &objs, std::set<std::pair<int, int>> const collisions, frac delta) {
+void calcCollisions(Array<obj> &objs, std::set<std::pair<int, int>> const collisions, double delta) {
   for (auto &&[a, b] : collisions) {
-    const frac va = objs[a].velocity, vb = objs[b].velocity;
+    const double va = objs[a].velocity, vb = objs[b].velocity;
 
     if (objs[a]._type == obj::obj_type::STATIC)
-      objs[b].velocity *= frac(-1);
+      objs[b].velocity *= double(-1);
     else
-      objs[a].velocity = (va * (objs[a]._m - objs[b]._m) + frac(2) * objs[b]._m * vb) / (objs[a]._m + objs[b]._m);
+      objs[a].velocity = (va * (objs[a]._m - objs[b]._m) + double(2) * objs[b]._m * vb) / (objs[a]._m + objs[b]._m);
     if (objs[b]._type == obj::obj_type::STATIC)
-      objs[a].velocity *= frac(-1);
+      objs[a].velocity *= double(-1);
     else
       objs[b].velocity = va - vb + objs[a].velocity;
 
-    frac x;
-    frac time;
+    double x;
+    double time;
     bool flag = false;
-    if (va.abs() < vb.abs())
-      flag = frac(0) < vb;
+    if (abs(va) < abs(vb))
+      flag = 0 < vb;
     else
-      flag = va < frac(0);
+      flag = va < 0;
 
     if (flag)
-      x = objs[b]._x + (vb < frac(0) ? frac(0) : objs[b]._w) - objs[a]._x;
+      x = objs[b]._x + (vb < double(0) ? double(0) : objs[b]._w) - objs[a]._x;
     else
-      x = objs[a]._x + (va < frac(0) ? frac(0) : objs[a]._w) - objs[b]._x;
+      x = objs[a]._x + (va < double(0) ? double(0) : objs[a]._w) - objs[b]._x;
 
-    time = x / (va - vb).abs();
+    time = x / abs(va - vb);
 
     objs[a]._x += -time * va + (delta - time) * objs[a].velocity;
     objs[b]._x += -time * vb + (delta - time) * objs[b].velocity;
     delta -= time;
 
-    Console << U"va:" << va.calc();
-    Console << U"vb:" << vb.calc();
-    Console << U"vad:" << objs[a].velocity.calc();
-    Console << U"vbd:" << objs[b].velocity.calc();
-    Console << U"x:" << x.calc();
-    Console << U"time:" << time.calc();
+    Console << U"va:" << va;
+    Console << U"vb:" << vb;
+    Console << U"vad:" << objs[a].velocity;
+    Console << U"vbd:" << objs[b].velocity;
+    Console << U"x:" << x;
+    Console << U"time:" << time;
     // Console << U"a pos:" << Unicode::Widen(objs[a]._x.get_top().str()) << '/' << Unicode::Widen(objs[a]._x.get_bottom().str());
-    Console << U"a pos:" << (objs[a]._x.get_top().str() + '/' + objs[a]._x.get_bottom().str()).size();
-    Console << U"a pos:" << objs[a]._x.calc();
+    // Console << U"a pos:" << (objs[a]._x.get_top().str() + '/' + objs[a]._x.get_bottom().str()).size();
+    Console << U"a pos:" << objs[a]._x;
     Console << ' ';
   }
 }
 
 int count = 0;
-int update(Array<obj> &objs) {
-  int collisionCount = 0;
+void update(Array<obj> &objs) {
   std::set<std::pair<int, int>> collisions;
-  frac delta = frac(Scene::DeltaTime());
+  double delta = double(Scene::DeltaTime());
   for (int i = 0; i < objs.size(); i++)
     objs[i]._x += objs[i].velocity * delta;
 
   collisions = getCollisions(objs);
-  collisionCount += collisions.size();
+  count += collisions.size();
   while (collisions.size() && System::Update()) {
     calcCollisions(objs, collisions, delta);
     collisions = getCollisions(objs);
@@ -128,24 +121,21 @@ int update(Array<obj> &objs) {
 
     ClearPrint();
     Print << objs[1].rect();
-    Print << objs[1].velocity.calc();
+    Print << objs[1].velocity;
     Print << Profiler::FPS();
     Print << count;
   }
-
-  return collisionCount;
 }
 
 void Main() {
   Window::Resize(1000, 600);
 
   Array<obj> objs;
-  objs << obj(frac(100), frac(100), frac(3), frac(400), frac(1));
-  objs << obj(frac(500), frac(250), frac(100), frac(100), frac(10000)).setVelocity(frac(-50)).setType(obj::obj_type::DYNAMIC);
-  objs << obj(frac(300), frac(250), frac(100), frac(100), frac(1)).setVelocity(frac(0)).setType(obj::obj_type::DYNAMIC);
+  objs << obj(double(100), double(100), double(3), double(400), double(1));
+  objs << obj(double(500), double(250), double(100), double(100), double(10000)).setVelocity(double(-50)).setType(obj::obj_type::DYNAMIC);
+  objs << obj(double(300), double(250), double(100), double(100), double(1)).setVelocity(double(0)).setType(obj::obj_type::DYNAMIC);
 
   while (System::Update()) {
-    // if (KeyEnter.down())
     update(objs);
 
     for (size_t i = 0; i < objs.size(); i++)
@@ -153,7 +143,7 @@ void Main() {
 
     ClearPrint();
     Print << objs[1].rect();
-    Print << objs[1].velocity.calc();
+    Print << objs[1].velocity;
     Print << Profiler::FPS();
     Print << count;
   }
